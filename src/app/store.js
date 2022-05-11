f151b75fce2290a58d28c5c9243dfa2d6857d241
Raymond Gottsch <rgottsch@logitech.com>
2022-05-11 09:01:34 -0700

    ignore updated

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import subRedditsReducer from "../features/subreddits/subRedditsSlice";
 import postsReducer from "../features/posts/postsSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     subreddits: subRedditsReducer,
     posts: postsReducer,
   },
